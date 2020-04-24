9e03247bec41272adf8b6254ebb987617bcfeb73
Greg Donald <gdonald@gmail.com>
2020-04-24 17:33:57 -0500

    [CH-2] Calculate approximate population

--- app/models/population.rb
+++ app/models/population.rb
@@ -20,10 +20,24 @@ class Population < ApplicationRecord
     pop = Population.find_by(year: year)
     unless pop
       max = Population.max
+
+      return Population.approximate(year) if max && year.between?(min.year, max.year)
+
       pop = max if max && year > max.year
       pop = min if pop.nil? && min && year > min.year
     end
 
     pop&.population
+  end
+
+  def self.approximate(year)
+    before = Population.where('year <= ?', year).order(:year).first
+    after = Population.where('year >= ?', year).order(year: :desc).first
+    return unless before && after
+
+    years = after.year - before.year
+    per_year = (after.population - before.population) / years.to_f
+
+    before.population + ((year - before.year) * per_year).to_i
   end
 end
